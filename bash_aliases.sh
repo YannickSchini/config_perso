ea2597550fba4abcef679b072ada23c44005e12e
Yannick Schini <yschini@octo.com>
2020-05-16 22:28:09 +0200

    feat(git alias): Usage of fzf in some of the git aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Git aliases
-alias gco='git checkout'
 alias gst='git status'
 alias gci='git commit'
-alias gbr='git branch'
 alias glola='git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)" --all'
 alias gpull='git pull --prune --rebase'
 alias grbi='git rebase -i'
@@ -14,10 +12,8 @@ alias ga='git add'
 
 if [ -f "/usr/share/bash-completion/completions/git" ]; then
   source /usr/share/bash-completion/completions/git
-  __git_complete gco _git_checkout
   __git_complete gst _git_status
   __git_complete gci _git_commit
-  __git_complete gbr _git_branch
   __git_complete glola _git_log
   __git_complete gpull _git_pull
   __git_complete grbi _git_rebase
