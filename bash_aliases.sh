c06428e7f15e4d09d62d9af3bb4fc4f2120b341c
Yannick Schini <yschini@octo.com>
2020-06-22 10:57:07 +0200

    feat: Update to git alias

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -2,7 +2,7 @@
 
 # Git aliases
 alias gst='git status'
-alias gci='git commit'
+alias gci='git commit -m'
 alias glola='git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)" --all'
 alias gpull='git pull --prune --rebase'
 alias grbi='git rebase -i'
