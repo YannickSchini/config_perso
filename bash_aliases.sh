93ac9bd87478fe9a43288615ed0e524c17e4c75f
Yannick Schini <yschini@octo.com>
2020-07-16 08:29:21 +0200

    feat: Additionnal git aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -3,7 +3,9 @@
 # Git aliases
 alias gst='git status'
 alias gci='git commit -m'
+alias glog='git log --graph --oneline'
 alias glola='git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)" --all'
+alias gf='git fetch'
 alias gpull='git pull --prune --rebase'
 alias grbi='git rebase -i'
 alias grba='git rebase --abort'
@@ -16,6 +18,8 @@ if [ -f "/usr/share/bash-completion/completions/git" ]; then
   source /usr/share/bash-completion/completions/git
   __git_complete gci _git_commit
   __git_complete glola _git_log
+  __git_complete glog _git_log
+  __git_complete gf _git_fetch
   __git_complete gpull _git_pull
   __git_complete grbi _git_rebase
   __git_complete grba _git_rebase
