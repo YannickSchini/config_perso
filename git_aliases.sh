4fedae401e905b7d9cacffd76eac7ae03b4f505b
yannick.schini <yannick.schini-octo@interieur.gouv.fr>
2020-07-27 10:58:50 +0200

    feat: Addition & refactoring of git variables
    Addition of diff commands
    Global refactoring of commands & aliases

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,37 +1,50 @@
 #!/bin/bash
 
-# Aliases
+# ALIASES
+
+# Misc
 alias gst='git status'
 alias gci='git commit'
-alias gf='git fetch'
-alias gpull='git pull --prune --rebase'
-alias grbi='git rebase -i'
-alias grba='git rebase --abort'
-alias grbc='git rebase --continue'
+alias grb='git rebase'
 alias ga='git add'
 alias gbr='git branch'
 alias gcob='git checkout -b'
-alias gsl='git log --oneline --decorate -30'
-alias gsla='git log --oneline --decorate --graph --all -30'
+
+# Distant repo
+alias gf='git fetch'
+alias gpush='git push'
+alias gpfwl='git push --force-with-lease'
+alias gpull='git pull --prune --rebase'
+
+# Git logging
+alias gsl='git log --oneline --decorate -40'
+alias gsla='git log --oneline --decorate --graph --all -25'
 alias gslap='git log --oneline --decorate --graph --all --numstat'
+
+# Git diff
+alias gd='git diff'
+alias gdm='git diff master'
+alias gdc='git diff HEAD^'
 
 if [ -f "/usr/share/bash-completion/completions/git" ]; then
   source /usr/share/bash-completion/completions/git
   __git_complete gci _git_commit
-  __git_complete glola _git_log
-  __git_complete glog _git_log
-  __git_complete gf _git_fetch
-  __git_complete gpull _git_pull
-  __git_complete grbi _git_rebase
-  __git_complete grba _git_rebase
-  __git_complete grbc _git_rebase
-  __git_complete ga _git_add
+  __git_complete grb _git_rebase
   __git_complete gbr _git_branch
+  __git_complete ga _git_add
+  __git_complete gf _git_fetch
+  __git_complete gpush _git_push
+  __git_complete gpfwl _git_push
+  __git_complete gpull _git_pull
+  __git_complete gsl _git_log
+  __git_complete gsla _git_log
+  __git_complete gslap _git_log
 else
   echo "Error loading git completions"
 fi
 
-# Functions
+# FUNCTIONS
+
 function gco() {
     if [ -z "$1" ]
     then
