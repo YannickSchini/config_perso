20667185c9d546f7becdd2f7a9d9046629608db8
Yannick Schini <yannick.schini@octo.com>
2020-09-09 22:37:10 +0200

    feat: Better error handling for of and ff functions

--- bash_config.sh
+++ bash_config.sh
@@ -5,9 +5,9 @@ function of() {
     if [ -z "$1" ]
     then
         if [ -x "$(command -v bat)" ]; then
-            vim $(fzf -m --reverse --preview "bat --style=numbers,changes --theme 'Monokai Extended' --color=always {}")
+            FILES=$(fzf -m --reverse --preview "bat --style=numbers,changes --theme 'Monokai Extended' --color=always {}") && vim "$FILES"
         else
-            vim $(fzf -m --reverse --preview "cat {}")
+            FILES=$(fzf -m --reverse --preview "cat {}") && vim "$FILES"
         fi
     else
         vim $1
@@ -22,9 +22,9 @@ function ff() {
         echo "Find File utility, usage: ff <string to be searched>"
     else
         if [ -x "$(command -v rg)" ] && [ -x "$(command -v bat)" ] && [ -x "$(command -v fzf)" ]; then
-            vim $(rg $1 --files-with-matches . | fzf -m --reverse --preview "bat --style=numbers,changes --theme 'Monokai Extended' --color=always {}")
+            FILES=$(rg $1 --files-with-matches . | fzf -m --reverse --preview "bat --style=numbers,changes --theme 'Monokai Extended' --color=always {}") && vim "$FILES"
         elif [ -x "$(command -v rg)" ] && [ ! -x "$(command -v bat)" ] && [ -x "$(command -v fzf)" ]; then
-            vim $(rg $1 --files-with-matches . | fzf -m --reverse --preview "cat {}")
+            FILES=$(rg $1 --files-with-matches . | fzf -m --reverse --preview "cat {}") && vim "$FILES"
         else
             echo "Please install the required libraries (ripgrep, fzf)"
         fi
